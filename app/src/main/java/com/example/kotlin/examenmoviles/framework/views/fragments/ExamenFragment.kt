a102e5ef74b1c1bd4dfbfe71cd00b2c9baf8a0e3
M4uricio2002 <a01703337@tec.mx>
2024-11-21 15:01:54 -0600

    feat: Agregue filtros de fecha y de lugar

--- app/src/main/java/com/example/kotlin/examenmoviles/framework/views/fragments/ExamenFragment.kt
+++ app/src/main/java/com/example/kotlin/examenmoviles/framework/views/fragments/ExamenFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -17,6 +19,9 @@ class ExamenFragment : Fragment() {
 
     private lateinit var viewModel: ExamenViewModel
     private lateinit var eventAdapter: EventAdapter
+    private lateinit var placeSpinner: Spinner
+    private lateinit var dateRangeSpinner: Spinner
+    private lateinit var applyFilterButton: Button
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -34,18 +39,31 @@ class ExamenFragment : Fragment() {
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
         recyclerView.adapter = eventAdapter
 
+        placeSpinner = view.findViewById(R.id.placeSpinner)
+        dateRangeSpinner = view.findViewById(R.id.dateRangeSpinner)
+        applyFilterButton = view.findViewById(R.id.applyFilterButton)
+
         viewModel = ViewModelProvider(requireActivity()).get(ExamenViewModel::class.java)
 
         viewModel.eventos.observe(viewLifecycleOwner) { eventos ->
             eventAdapter.updateData(eventos)
         }
 
+        viewModel.filteredEvents.observe(viewLifecycleOwner) { filtered ->
+            eventAdapter.updateData(filtered)
+        }
+
         viewModel.error.observe(viewLifecycleOwner) { mensajeError ->
             Toast.makeText(requireContext(), mensajeError, Toast.LENGTH_LONG).show()
         }
 
-        viewModel.filteredEvents.observe(viewLifecycleOwner) { filtered ->
-            eventAdapter.updateData(filtered)
+        applyFilterButton.setOnClickListener {
+            val selectedPlace = placeSpinner.selectedItem.toString()
+            val selectedRange = dateRangeSpinner.selectedItem.toString()
+            viewModel.filterEvents(
+                if (selectedPlace == "Todos") null else selectedPlace,
+                selectedRange
+            )
         }
 
         viewModel.fetchEvents(page = 1)
